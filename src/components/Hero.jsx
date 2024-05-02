7e5ead269e7140519d05ec96d6653b5546b21e9a
Nole Kjemhus <97925558+Kjemhus@users.noreply.github.com>
2024-05-02 12:03:53 -0600

    welcome text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}> 
+        <div className="flex flex-col justify-center items-center mt-5"> 
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" /> {/* circle */}
+          <div className="w-1 sm:h-80 h-40 violet-gradient" /> {/* line */}
+        </div>
+
+        {/* Welcome Text */}
+        <div> 
+          <h1 className={`${styles.heroHeadText} text-white`}> Hi, I'm <span className="text-[#915eff]"> Nole </span></h1>
+          <p className={`${styles.heroSubText} mt -2 text-white-100`}> 
+            I study Computing Science and Mathematics <br className="sm:block hidden" /> at the University of Alberta
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas /> 
+
+    </section>
   )
 }
 
